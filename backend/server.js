94fbc11a2e72bad1b47d65e54e31e5c51fe7d222
Metakilovs <joymwangi1255@gmail.com>
2026-07-04 20:24:26 +0300

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const bookingRoutes = require("./routes/bookings");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://tradefinder-dusky.vercel.app", "http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json());
 
 connectDB();
